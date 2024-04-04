48a747a98375ca34adcf02e1346ca1d681d805af
Slavinto <slava3669@gmail.com>
2024-04-04 17:12:19 +0300

    fixed a minor bug with form reset link

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,14 +1,10 @@
 "use server";
 
-import Email from "next-auth/providers/email";
 import { getUser, updateUser } from "./prisma-crud";
-import { json } from "stream/consumers";
 
 export const submitSettingsForm = async (email: string, formData: FormData) => {
     const name = formData.get("name")?.toString().trim();
-    // const email = email.toString().trim();
     const colorScheme = formData.get("color")?.toString();
-    console.log(name, email, colorScheme);
     if (!name || !email || !colorScheme) {
         throw new Error("Error. Invalid form data.");
     }
